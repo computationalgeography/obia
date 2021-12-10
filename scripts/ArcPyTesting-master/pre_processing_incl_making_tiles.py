7bc4d832373734e91988961d19790fb4ba990a53
Edwin Sutanudjaja <edwinkost@users.noreply.github.com>
2021-12-10 01:41:59 +0100

    Add files via upload

--- scripts/ArcPyTesting-master/pre_processing_incl_making_tiles.py
+++ scripts/ArcPyTesting-master/pre_processing_incl_making_tiles.py
@@ -8,30 +8,35 @@ sys.path.append(r'c:\edwin\scripts\ArcPyTesting-master')
 from project_tools.prep_auto_tools import *
 import os
 
-# =============================================================================
-# ## Create mosaic ##
-# =============================================================================
-# set in and output paths
+
 # gdal_bat_arg = "C:\Program Files\QGIS 3.16.8\OSGeo4W.bat"
 # gdal_bat = r'"{}"'.format(gdal_bat_arg)
 #
-# We don't need this as Edwin has modified the scripts such that it will just use gdal functions loaded by the gdal conda environment.
+# We don't need the following (gdal_bat) as Edwin has modified the scripts such that they will just use gdal functions (e.g. that can loaded in a gdal conda environment).
 gdal_bat = None
 
+
+# set in and output paths
 
 root = r'C:\edwin\scratch\rws_obia_2018_test'
 data_folder = os.path.join(root, '01_data')
 images_folder = os.path.join(data_folder, '01_tegels_luchtfoto')
-## edwin's question: What are the data_folder and images_folder?
 
 project_name = 'Westerschelde_2018'
 output_folder = os.path.join(root, '02_Mozaiek')
 mosaic_name = os.path.join(output_folder, '{}.tif'.format(project_name))
 
+
+
+# =============================================================================
+# ## Create mosaic ##
+# =============================================================================
+
 # create mosaic in vrt
 output_mosaic_name = os.path.join(output_folder, '{}'.format(project_name))
 create_vrt_mosaic(images_folder, output_folder, output_mosaic_name)
 mosaic_name = output_mosaic_name + ".vrt"
+
 
 # =============================================================================
 # ## Create Tiles ##
@@ -63,6 +68,7 @@ tile_extents_path = os.path.join(tile_folder, '01b_tiles_index')
 tile_extents_name = 'TileIndex.shp'
 create_tile_extents(output_tile_folder, tile_extents_path, tile_extents_name)
 
+
 # Creating tile extents shapefile no overlap
 input_path = output_tile_folder
 output_path = os.path.join(tile_folder, '1c_tiles_index_no_overlap')
@@ -84,3 +90,5 @@ hoogte_raster  = os.path.join(data_folder, '02_hoogte', 'ws18_2m_tot.tif')
 nieuw_raster_path = os.path.join(tile_folder, '02_tiles_hoogte')
 
 raster_subset_with_tileindex_tiles(shapefile_path, hoogte_raster, nieuw_raster_path)
+
+
